9bef6546cf787acf1b908bf5ab2649904be5c3cd
amund-fremming <amund.fremming@gmail.com>
2024-01-16 17:51:19 +0100

    Added interfaces for all controllers

---
+++ backend/Enums/Type.cs
@@ -0,0 +1,12 @@
+namespace Enums;
+
+/// <summary>
+/// Enum for Type in EventRelation
+/// </summary>
+public enum Type
+{
+    JOINED,
+    DECLINED,
+    PENDING,
+    BAILED
+}
--- backend/Interfaces/Controllers/IEventRelationController.cs
+++ backend/Interfaces/Controllers/IEventRelationController.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace Interfaces.Controllers;
 
-public class IEventRelationController
+/// <summary>
+/// Controller Interface for updating EventRelations
+/// </summary>
+public interface IEventRelationController
 {
-    
+    Task<ActionResult> UpdateEventRelationType([FromQuery] string eventId, [FromQuery] string userId, [FromQuery] string type);
+
+    Task<ActionResult> UpdateEventRelationRole([FromQuery] string eventId, [FromQuery] string userId, [FromQuery] string role);
 }
