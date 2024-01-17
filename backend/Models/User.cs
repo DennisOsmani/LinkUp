d865f7b164a4ad788c919c4afb3e3429ac2c2e36
adburg <adrian.berget@gmail.com>
2024-01-17 17:20:31 +0100

    Model and enum User
    Added enum for user role, and added some code for user model, WIP

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -1,6 +1,31 @@
+using Enums;
 namespace Models;
+
+//TODO:
+//fix warnings with nullability and constuctor, add relation tables, see over ENUM configuration, ENUM for relationship status? Key, length etc. add
 
 public class User
 {
+    public string UserId { get; set; }
+    public string Username { get; set; }
+    public string Firstname { get; set; }
+    public DateTime DateBorn { get; set; }
+    public string Phone { get; set; }
+    public string RelationshipStatus { get; set; }
+    public string Gender { get; set; }
+    public string Description { get; set; }
+    public string Email { get; set; }
+    public string ProfileImage { get; set; }
+    public string Password { get; set; }
+    public string Salt { get; set; }
+    public int EventsCreated { get; set; }
+    public int EventsJoined { get; set; }
+    public int EventBails { get; set; }
+    public Role Role { get; set; }
 
+    public User(string UserId)
+    {
+        this.UserId = UserId;
+
+    }
 }
