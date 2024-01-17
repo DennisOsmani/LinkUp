a1d7a1abfeca5115e6a9060818bc0f8339184093
amund-fremming <amund.fremming@gmail.com>
2024-01-17 17:34:29 +0100

    Finished UserRelation

--- backend/Models/UserRelation.cs
+++ backend/Models/UserRelation.cs
@@ -1,6 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using Enums;
+
 namespace Models;
 
 public class UserRelation
 {
+    [Key]
+    public int UserRelationId { get; set; }
+    public int User_first_id { get; set; }
+    public User? User_first { get; set; }
+    public int User_second_id { get; set; }
+    public User? User_second { get; set; }
+    public UserRelationType Type { get; set; }
 
+    public UserRelation(int user_first_id, int user_second_id, UserRelationType type)
+    {
+        this.User_first_id = user_first_id;
+        this.User_second_id = user_second_id;
+        this.Type = type;
+    }
 }
