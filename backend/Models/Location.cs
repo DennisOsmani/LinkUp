e3ba864dd5a155aab6487a6008e18d241132cf57
amund-fremming <amund.fremming@gmail.com>
2024-01-17 19:11:28 +0100

    Added objects to Location and EventRelation

--- backend/Models/Location.cs
+++ backend/Models/Location.cs
@@ -5,13 +5,14 @@ public class Location
 {
     [Key]
     public int LocationID { get; set; }
-    public Event EventID { get; set; }
+    public int EventID { get; set; }
+    public Event? Event { get; set; }
     public string Address { get; set; }
     public string Postalcode { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
 
-    public Location(Event eventId, string address, string postalcode, string city, string country) 
+    public Location(int eventId, string address, string postalcode, string city, string country) 
     {
         this.EventID = eventId;
         this.Address = address;
