6c10070b2f9eb81b4758c31aeb3b1a045ae3dc0f
amund-fremming <amund.fremming@gmail.com>
2024-01-17 10:45:08 +0100

    Added docs to all interfaces

--- backend/Interfaces/IEventService.cs
+++ backend/Interfaces/IEventService.cs
@@ -8,19 +8,58 @@ namespace Interfaces;
 
 public interface IEventService
 {
+    /// <summary>
+    /// Fetches a list of Events where the user has the UserRelation Friends to the creator and visibility set to friends.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns>A list of Events</returns>
     Task<List<Event>> GetUserFriendEvents([FromQuery] string userId);
 
+    /// <summary>
+    /// Fetches a list of Events based on what city you are in.
+    /// </summary>
+    /// <param name="city"></param>
+    /// <returns>A list of Events</returns>
     Task<List<Event>> GetEvetsInCity([FromQuery] string city);
 
+    /// <summary>
+    /// Fetches a Event by a given id.
+    /// </summary>
+    /// <param name="eventId"></param>
+    /// <returns>A Event</returns>
     Task<Event> GetEvent([FromQuery] string eventId); // Maybe also fetch a list of users in the event
 
-    Task<List<Event>> GetUserEventsByStatus([FromQuery] string type);
+    /// <summary>
+    /// Fetches a list of Events based on what type the EventRelation is.
+    /// </summary>
+    /// <param name="type">JOINED, DECLINED, PENDING, BAILED</param>
+    /// <returns>A list of Events</returns>
+    Task<List<Event>> GetUserEventsByType([FromQuery] string type);
 
-    Task CreateEvent([FromBody] Event newEvent);
+    /// <summary>
+    /// Creates a new Event.
+    /// </summary>
+    /// <param name="newEvent">Event Object</param>
+    /// <returns>Newly created Event<returns>
+    Task<Event> CreateEvent([FromBody] Event newEvent);
 
-    Task UpdateEvent([FromBody] Event updatedEvent);
+    /// <summary>
+    /// Updates a Event.
+    /// </summary>
+    /// <param name="updatedEvent">Event Object</param>
+    /// <returns>Updated Event<returns> 
+    Task<Event> UpdateEvent([FromBody] Event updatedEvent);
 
-    Task UpdateEventLocation([FromBody] Event updatedEvent);
+    /// <summary>
+    /// Updates.
+    /// </summary>
+    /// <param name="updatedEvent">EventObject</param>
+    /// <returns>Updated Event</returns>
+    Task<Event> UpdateEventLocation([FromQuery] string eventId, [FromBody] Location location);
 
+    /// <summary>
+    /// Deletes a Event.
+    /// </summary>
+    /// <param name="eventId"></param>
     Task DeleteEvent([FromQuery] string eventId);
 }
