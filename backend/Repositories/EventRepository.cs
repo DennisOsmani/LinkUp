c6512f1a69466fc5d000d7440698a5793032b5c0
DennisOsmani <dennisosmani@hotmail.com>
2024-01-18 14:45:52 +0100

    created the repo class

---
+++ backend/Repositories/EventRepository.cs
@@ -0,0 +1,14 @@
+namespace Repositories;
+using Enums;
+using Models;
+using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// Repository for handling Event updates and manipulation to Event table.
+/// </summary>
+public class EventRepository 
+{
+    public readonly AppDbContext _context;
+
+
+}
