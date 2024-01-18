e19402baf4316dc35ada9d5b55450991cebb37b9
DennisOsmani <dennisosmani@hotmail.com>
2024-01-18 14:23:16 +0100

    fix the event-model

--- backend/Models/Event.cs
+++ backend/Models/Event.cs
@@ -6,6 +6,7 @@ public class Event
     [Key]
     public int EventID { get; set; }
     public string CreatorUserID { get; set; }
+    public User? Useer { get; set; }
     public string EventName { get; set; }
     public string EventDescription { get; set; }
     public DateTime EventDateTimeStart { get; set; }
@@ -15,6 +16,7 @@ public class Event
     public string? FrontImage { get; set; }
     public string? MinCapacity { get; set; }
     public string? MaxCapacity { get; set; }
+    public Location? Location { get; set; }
 
     public Event(string creatorUserID, string eventName, string eventDescription, 
         DateTime eventDateTimeStart, DateTime eventDateTimeEnd, string inviteURL)
