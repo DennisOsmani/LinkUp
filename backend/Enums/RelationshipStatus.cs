e47343074a9d2ffcd074da9d8055dbd56a3d7a5b
adburg <adrian.berget@gmail.com>
2024-01-18 14:34:27 +0100

    added relationship enum and finished user model with some insecurities

---
+++ backend/Enums/RelationshipStatus.cs
@@ -0,0 +1,12 @@
+namespace Enums;
+
+/// <summary>
+/// Enum for Relationshipstatus for User
+/// </summary>
+public enum RelationshipStatus
+{
+    MARRIED,
+    RELATIONSHIP,
+    SINGLE,
+    COMPLICATED
+}
