2018cd0f73e58232f2c2566cbdafe625aab047f1
adburg <adrian.berget@gmail.com>
2024-01-18 15:46:03 +0100

    updates
    updates user model

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -6,9 +6,9 @@ public class User
 {
     [Key]
     public string UserID { get; set; }
-    public string Username { get; set; }
-    public string? Firstname { get; set; }
-    public string? Lastname { get; set; }
+    public string? Username { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
     public DateTime? DateBorn { get; set; }
     public string? Phone { get; set; }
     public RelationshipStatus? RelationshipStatus { get; set; }
@@ -16,22 +16,21 @@ public class User
     public string? Description { get; set; }
     public string Email { get; set; }
     public string? ProfileImage { get; set; }
-    public string Password { get; set; }
-    public string Salt { get; set; }
+    public string? Password { get; set; }
+    public string? Salt { get; set; }
     public int? EventsCreated { get; set; }
     public int? EventsJoined { get; set; }
     public int? EventBails { get; set; }
     public Role Role { get; set; }
-    public ICollection<UserRelation>? FirstUserRelations { get; set; } //SecondUserRelations???
-    public ICollection<EventRelation>? EventRelation { get; set; }
+    public ICollection<UserRelation>? UserRelations { get; set; }
+    public ICollection<EventRelation>? EventRelations { get; set; }
 
-    public User(string userId, string username, string email, string password, string salt)
+    public User(string userId, string firstname, string lastname, string email)
     {
         this.UserID = userId;
-        this.Username = username;
+        this.Firstname = firstname;
+        this.Lastname = lastname;
         this.Email = email;
-        this.Password = password;
-        this.Salt = salt;
         this.Role = Role.USER;  //bruuh
 
     }
