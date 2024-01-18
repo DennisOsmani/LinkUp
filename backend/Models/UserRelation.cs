53c0ae2ae66a1acbad6bd81e0895462b9bf55ffd
amund-fremming <amund.fremming@gmail.com>
2024-01-18 16:07:37 +0100

    Fixed some shit

--- backend/Models/UserRelation.cs
+++ backend/Models/UserRelation.cs
@@ -6,17 +6,17 @@ namespace Models;
 public class UserRelation
 {
     [Key]
-    public int UserRelationId { get; set; }
-    public string User_first_id { get; set; }
+    public int UserRelationID { get; set; }
+    public string User_first_ID { get; set; }
     public User? User_first { get; set; }
-    public string User_second_id { get; set; }
+    public string User_second_ID { get; set; }
     public User? User_second { get; set; }
     public UserRelationType Type { get; set; }
 
     public UserRelation(string user_first_id, string user_second_id, UserRelationType type)
     {
-        this.User_first_id = user_first_id;
-        this.User_second_id = user_second_id;
+        this.User_first_ID = user_first_id;
+        this.User_second_ID = user_second_id;
         this.Type = type;
     }
 }
