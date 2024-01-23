dc13937385c7b77bbf7e764598f1d2ed2a44a5a0
amund-fremming <amund.fremming@gmail.com>
2024-01-23 17:42:37 +0100

    Started on controllers

--- backend/Controllers/UserRelationController.cs
+++ backend/Controllers/UserRelationController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Services;
+using Models;
 
 namespace Controllers;
 
@@ -6,5 +8,38 @@ namespace Controllers;
 [Route("api/userrelation")]
 public class UserRelationController : ControllerBase
 {
-    
+    public readonly UserRelationService _urService;
+
+    public UserRelationController(UserRelationService urService)
+    {
+        _urService = urService;
+    }
+
+    [HttpPut("/update")]
+    public async Task<IActionResult> UpdateUserRelationType([FromQuery] string userId, [FromQuery] string otherUserId, [FromQuery] string type)
+    {
+        UserRelation updatedUserRelation;
+
+        try
+        {
+            updatedUserRelation = await _urService.UpdateUserRelationType();
+        }
+        catch(Exception e)
+        {
+
+        }
+    }
+
+    [HttpPost("/create")]
+    public async Task<IActionResult> CreateUserRelation([FromQuery] string userId, [FromQuery] string otherUserId, [FromQuery] string type)
+    {
+        try
+        {
+
+        }
+        catch(Exception e)
+        {
+
+        }
+    }
 }
