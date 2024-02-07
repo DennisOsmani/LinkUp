5f83c4903b62f8e07766ec7227c9378f7789d390
adburg <adrian.berget@gmail.com>
2024-02-07 09:53:29 +0100

    Interfaces updated
    Fixed all interfaces to match services

--- backend/Interfaces/IUserService.cs
+++ backend/Interfaces/IUserService.cs
@@ -15,13 +15,24 @@ public interface IUserService
     /// <returns>A User</returns>
     Task<User?> GetUser(string userId);
 
-
     /// <summary>
     /// Creates a new user and saves it in the database
     /// </summary>
     /// <param name="user"></param>
     Task CreateUser(User user);
 
+    /// <summary>
+    /// Updates a User.
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns>Updated User</returns>
+    Task<User?> UpdateUser(string userId, User updatedUser);
+
+    /// <summary>
+    /// Deletes a User.
+    /// </summary>
+    /// <param name="userId"></param>
+    Task DeleteUser(string userId);
 
     /// <summary>
     /// Fetches a list of User based on a search string for their names.
@@ -29,19 +40,6 @@ public interface IUserService
     /// <param name="fullName"></param>
     /// <returns>A list of Users</returns>
     Task<ICollection<User>> SearchUsers(string searchString);
-
-    /// <summary>
-    /// Updates a User.
-    /// </summary>
-    /// <param name="user"></param>
-    /// <returns>Updated User</returns>
-    Task<User?> UpdateUser(string userId, User newUser);
-
-    /// <summary>
-    /// Deletes a User.
-    /// </summary>
-    /// <param name="userId"></param>
-    Task DeleteUser(string userId);
 
     /// <summary>
     /// Fetches a list of Users that have joined a event, by the event id.
