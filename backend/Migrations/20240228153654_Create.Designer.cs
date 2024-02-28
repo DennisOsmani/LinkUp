9abb2dac01c8e25dcaf7b8bc2b63644829e6ebfd
amund-fremming <amund.fremming@gmail.com>
2024-02-28 16:37:08 +0100

    :bug:

--- backend/Migrations/20240228153654_Create.Designer.cs
+++ backend/Migrations/20240228153654_Create.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20240228153034_Create")]
+    [Migration("20240228153654_Create")]
     partial class Create
     {
         /// <inheritdoc />
