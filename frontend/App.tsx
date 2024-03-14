cc3e16c6785f98445a637a6a09c3dbe3065d85e9
amund-fremming <amund.fremming@gmail.com>
2024-03-14 07:52:58 +0100

    Added fetch example and token provider

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TokenProvider } from './providers/TokenProvider';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <TokenProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </TokenProvider>
   );
 }
 
