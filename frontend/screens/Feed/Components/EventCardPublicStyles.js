40d18970a3921675ad45411842d110e3d8a9c30d
adburg <adrian.berget@gmail.com>
2024-03-14 17:34:50 +0100

    Almost finished eventcard
    Missing feather icons, and funtions

--- frontend/screens/Feed/Components/EventCardPublicStyles.js
+++ frontend/screens/Feed/Components/EventCardPublicStyles.js
@@ -1,85 +1,78 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageStyle } from "react-native";
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   card: {
     backgroundColor: "#ffffff",
-    borderRadius: 8,
-    padding: 15,
-    marginBottom: 10,
-    maxHeight: 240,
+    borderRadius: 16,
+    padding: 16,
+    gap: 10,
     width: "100%"
   },
   header: {
     flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 10
+    justifyContent: "space-between"
   },
-  title: {
-    fontSize: 22,
-    marginBottom: 5,
-    fontWeight: "bold"
-  },
-  smallText: {
-    fontSize: 12,
+  headerText: {
+    fontSize: 14,
     color: "gray"
-  },
-  addressText: {
-    fontSize: 12,
-    color: "gray",
-    marginBottom: 8
   },
   content: {
     flexDirection: "row",
-    marginBottom: 10
+    justifyContent: "space-between",
+    gap: 10,
+    height: 200,
+    display: "flex"
   },
   leftSide: {
-    flex: 1,
-    flexGrow: 2,
-    justifyContent: "space-between",
-    flexDirection: "column"
+    flex: "2",
+    flexDirection: "column",
+    justifyContent: "space-between"
   },
   upperLeftSide: {
-    alignItems: "flex-start",
-    marginBottom: "auto"
+    gap: 6
   },
-  lowerLeftSide: {
-    justifyContent: "flex-end",
-    marginBottom: "auto"
+  title: {
+    fontSize: 22,
+    fontWeight: "bold"
   },
   text: {
-    fontSize: 16,
-    marginBottom: 5
+    fontSize: 16
   },
-  rightSide: {
-    flex: 1,
-    flexGrow: 1,
-    justifyContent: "flex-end"
+  lowerLeftSide: {
+    gap: 8
   },
-  image: {
-    width: "100%",
-    height: 160, // Adjust height as needed
-    resizeMode: "cover",
-    borderRadius: 8
+  addressText: {
+    marginLeft: 3,
+    fontSize: 14,
+    color: "gray"
   },
-  largeButton: {
+  button: {
     display: "flex",
     justifyContent: "center",
-    alignItems: "center",
     borderRadius: 12,
     backgroundColor: "#474AEE",
-    height: "40%",
-    width: "70%"
+    height: 40,
+    width: 150
   },
-  largeButtonText: {
+  buttonText: {
     color: "#FFFFFF",
     textAlign: "center",
     fontSize: 18,
     fontWeight: "bold",
     fontFamily: ""
   },
+  rightSide: {
+    flex: 1
+  },
+  ///THIS DOES NOT WORK
+  image: {
+    height: "100%",
+    width: "100%",
+    resizeMode: "cover",
+    borderRadius: 16
+  },
+  /////////////////////
   smallButton: {
     borderRadius: 10
   }
 });
-
-export { styles };
