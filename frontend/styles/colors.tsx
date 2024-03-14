97726aa60d8aefdb03c6c7525aab1b40dc215ab1
DennisOsmani <dennisosmani@hotmail.com>
2024-03-14 14:28:20 +0100

    missing just the font

---
+++ frontend/styles/colors.tsx
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: "#474AEE",
+  secondary: "#DFE1F8",
+  thirdary: "#828299",
+  background: "#F1F2F6",
+  foreground: "#FFFFFF",
+  green: "#36AE46",
+  red: "#DE1240",
+  grey: "#808080",
+};
