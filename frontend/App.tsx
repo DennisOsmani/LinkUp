9bcf8746b852e23708440f475eace24e616f8e74
amund-fremming <amund.fremming@gmail.com>
2024-03-15 10:30:40 +0100

    Added fonts

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,21 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { TokenProvider } from './providers/TokenProvider';
+import { useState, useEffect } from 'react';
+import * as Font from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      BalooBold: require('./assets/fonts/Baloo2-Bold.ttf'),
+      BalooRegular: require('./assets/fonts/Baloo2-Regular.ttf'),
+    });
+
+    setFontsLoaded(true);
+  };
+
+  if (!fontsLoaded) {
+    // Load splash screen!
+    return (
+      <View>
+        <Text style={{ fontFamily: 'BalooBold' }}>Splash</Text>
+      </View>
+    );
+  }
+
   return (
     <TokenProvider>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+      <View>
+        <Text>App.tsx</Text>
       </View>
     </TokenProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
