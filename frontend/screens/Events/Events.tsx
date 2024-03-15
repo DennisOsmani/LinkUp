236739b4279efdc3d99eb13b2129c4df409a91dc
amund-fremming <amund.fremming@gmail.com>
2024-03-15 11:36:25 +0100

    Tab navigatoion iteration 1

--- frontend/screens/Events/Events.tsx
+++ frontend/screens/Events/Events.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native';
 
-export default function Feed() {
+export default function Events() {
   return (
     <View>
-      <Text>Feed</Text>
+      <Text>Events</Text>
     </View>
   );
 }
