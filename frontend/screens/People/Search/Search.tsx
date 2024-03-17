8c2275afc7912a22ea431f4e1315812de31f8b72
D596242 <Dennisosmani@hotmail.com>
2024-03-17 23:26:02 +0100

    clear search text function

--- frontend/screens/People/Search/Search.tsx
+++ frontend/screens/People/Search/Search.tsx
@@ -4,14 +4,25 @@ import { UserCard } from "../../../components/UserCard/UserCard";
 import styles from "../../People/Search/SearchStyles";
 import { Feather } from "@expo/vector-icons";
 import { useState } from "react";
+ 
 
 export default function SearchPeople() {
+  const [searchText, setSearchText] = useState("");
+
+  const clearSearchText = () => {
+    setSearchText("");
+  }
+
+  // Filter the userCArds baswed on the searchText
+  // Need USER_DATA or all USERS from API in backend, 
+  // so we can filter the cards out based on firstname and lastname
+  // const filterUserCards = 
   return (
     <ScrollView>
       <View style={styles.contentContainer}>
         <View style={styles.searchContainer}>
-          <TextInput style={styles.searchBar} placeholder="Søk"></TextInput>
-          <Feather style={styles.icon} name="x" />
+          <TextInput style={styles.searchBar} placeholder="Søk" onChangeText={setSearchText} value={searchText}></TextInput>
+          <Feather style={styles.icon} name="x" onPress={clearSearchText} />
         </View>
         <UserCard
           userCardInfo={{
