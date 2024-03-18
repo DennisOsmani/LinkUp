b66053e4029885d9766fcdbbc1d0e8bb42643a7a
amund-fremming <amund.fremming@gmail.com>
2024-03-18 23:33:36 +0100

    Login and register done, setToken bug

--- frontend/api/AuthAPI.ts
+++ frontend/api/AuthAPI.ts
@@ -1,4 +1,9 @@
-const BASE_URL = '/api/auth';
+const BASE_URL =
+  "https://4863-2001-4647-7170-0-2617-1924-9243-d6c6.ngrok-free.app/api/auth";
+
+export interface IToken {
+  token: string;
+}
 
 export interface IRegistrationRequest {
   firstname: string;
@@ -10,20 +15,21 @@ export interface IRegistrationRequest {
 export const registerUser = async (request: IRegistrationRequest) => {
   try {
     const response = await fetch(`${BASE_URL}/register`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(request),
     });
 
     if (!response.ok) {
-      throw new Error('Error in register request ' + response.status);
+      throw new Error("Error in register request " + response.status);
     }
 
-    return await response.json();
+    const data: IToken = await response.json();
+    return data;
   } catch (error) {
-    console.log('Error while registering a user ' + error);
+    throw new Error("Error while registering a user " + error);
   }
 };
 
@@ -35,19 +41,20 @@ export interface ILoginRequest {
 export const loginUser = async (request: ILoginRequest) => {
   try {
     const response = await fetch(`${BASE_URL}/login`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(request),
     });
 
     if (!response.ok) {
-      throw new Error('Error in login request ' + response.status);
+      throw new Error("Error in login request " + response.status);
     }
 
-    return await response.json();
+    const data: IToken = await response.json();
+    return data;
   } catch (error) {
-    console.log('Error while loggin in a user ' + error);
+    throw new Error("Error while loggin in a user " + error);
   }
 };
