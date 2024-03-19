6f0a51132381a9220fa04f18bc939fe8f2b257a3
Amund Fremming <amundfremming@Amunds-Mac-mini.local>
2024-03-19 18:33:36 +0100

    Fixed tab styling

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,13 +1,8 @@
-import { TokenProvider } from "./providers/TokenProvider";
 import LoginWallRouter from "./screens/LoginWallRouter/LoginWallRouter";
-
-export default function App() {
-import { Text, View } from "react-native";
-import { TokenProvider } from "./providers/TokenProvider";
 import { useState, useEffect } from "react";
 import * as Font from "expo-font";
-import TabNavigator from "./navigation/TabNavigator";
 import Splash from "./screens/Splash/Splash";
+import { TokenProvider } from "./providers/TokenProvider";
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState<boolean>(false);
@@ -22,7 +17,9 @@ export default function App() {
       BalooRegular: require("./assets/fonts/Baloo2-Regular.ttf"),
     });
 
-    setFontsLoaded(true);
+    setTimeout(() => {
+      setFontsLoaded(true);
+    }, 1000);
   };
 
   if (!fontsLoaded) {
