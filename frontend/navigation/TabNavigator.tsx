d568ec52f431bafcab0eb9c966e9caa525cac451
D596242 <Dennisosmani@hotmail.com>
2024-03-22 10:50:12 +0100

    Navbar sexy

--- frontend/navigation/TabNavigator.tsx
+++ frontend/navigation/TabNavigator.tsx
@@ -7,38 +7,60 @@ import Profile from "../screens/Profile/Profile";
 import Events from "../screens/Events/Events";
 import People from "../screens/People/People";
 import { Feather } from "@expo/vector-icons";
-import TabNavigatorStyles from "./TabNavigatorStyles";
+import styles from "./TabNavigatorStyles";
+import { useState } from "react";
+import { View } from "react-native";
 
 const Tab = createBottomTabNavigator<NavigationTypeList>();
 
 export default function TabNavigator() {
+  // const [isPressed, setIsPressed] = useState<boolean>(false);
+
+  // const handlePress = () => {
+  //   setIsPressed(true);
+  // }
+
   return (
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen
-          options={{ headerShown: false, tabBarIcon: () => (
-            <Feather style={TabNavigatorStyles.feed} name="home" ></Feather>
+          options={{ headerShown: false,  tabBarIcon: ({color}) => (
+            <Feather style={styles.icons} name="home" color={color}
+            />
           ) }}
           name="Feed"
           component={Feed}
+          
         /> 
         <Tab.Screen
-          options={{ headerShown: false }}
+          options={{ headerShown: false,  tabBarIcon: ({color}) => (
+            <Feather style={styles.icons} name="calendar" color={color}
+            />
+          ) }}
           name="Events"
           component={Events}
         />
         <Tab.Screen
-          options={{ headerShown: false }}
+          options={{ headerShown: false,  tabBarIcon: ({color}) => (
+            <Feather style={styles.plus} name="plus-square" color={color}
+            />
+          ) }}
           name="Create"
           component={CreateEvent}
         />
         <Tab.Screen
-          options={{ headerShown: false }}
+          options={{ headerShown: false,  tabBarIcon: ({color}) => (
+            <Feather style={styles.icons} name="users" color={color}
+            />
+          ) }}
           name="People"
           component={People}
         />
         <Tab.Screen
-          options={{ headerShown: false }}
+          options={{ headerShown: false,  tabBarIcon: ({color}) => (
+            <Feather style={styles.icons} name="user" color={color}
+            />
+          ) }}
           name="Profile"
           component={Profile}
         />
