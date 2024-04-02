bb6d3307281c09ae66dbbe85bd2c1a9a63ea315f
D596242 <Dennisosmani@hotmail.com>
2024-04-02 23:30:46 +0200

    started on other profile screen

--- frontend/screens/OtherProfile/OtherProfile.tsx
+++ frontend/screens/OtherProfile/OtherProfile.tsx
@@ -1,1 +1,25 @@
-// hehe
+import { View } from "react-native";
+import { TabBackground } from "../../components/TabBackground/TabBackground";
+
+interface OtherProfileProps {
+    image: string,
+    created: number,
+    joined: number,
+    bails: number,
+    firstname: string,
+    lastname: string,
+    gender: string,
+    age: number,
+    status: string,
+    bio: string,
+}
+
+export default function OtherProfile() {
+
+
+    return (
+        <View>
+            
+        </View>
+    );
+}
