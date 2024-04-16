7b95bbcd4fd469635797cae19c087a2cc567c72b
DennisOsmani <dennisosmani@hotmail.com>
2024-04-16 16:41:00 +0200

    start on Porfile screen

--- frontend/screens/Profile/Profile.tsx
+++ frontend/screens/Profile/Profile.tsx
@@ -1,9 +1,9 @@
-import { View, Text } from 'react-native';
+import { View, Text } from "react-native";
 
 export default function Profile() {
   return (
     <View>
-      <Text>Profile</Text>
+      <Text>Profile bby</Text>
     </View>
   );
 }
