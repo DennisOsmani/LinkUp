8c0844ad9c8b679b67c3e25672cb815fe4a7da77
adburg <adrian.berget@gmail.com>
2024-05-01 17:12:47 +0200

    url path again
    sorry

--- frontend/api/UrlPaths.ts
+++ frontend/api/UrlPaths.ts
@@ -1,4 +1,4 @@
-export const URL_BASE = "https://localhost:5173/api";
+export const URL_BASE = "http://localhost:5173/api";
 // export const URL_BASE = "https://linkupbackend.azurewebsites.net/api";
 
 export const EVENT_PATH = "/event";
